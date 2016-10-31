d3a4b41104f0d4c95045ab99797ee3fdea9f6bc3
kfrn <knfrances@gmail.com>
2016-11-01 11:36:24 +1300

    Render cats page with our list from db

--- routes/cats.js
+++ routes/cats.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var catsDB = require('../db/cats')
 
-router.get('/cats', function(req, res, next) {
-  res.render('respond with a resource');
+router.get('/', function(req, res, next) {
+  catsDB.getAllCats()
+    .then(function(catsFromDB){
+      res.render('cats', { cats: catsFromDB })
+    })
+    .catch(function(error){
+      res.status(500)
+      console.log(error);
+    })
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/cats', 200);
+  res.redirect('/cats');
 });
 
-module.exports = router;
+module.exports = router
