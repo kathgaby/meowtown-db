4c24ca3801eaa945f17e51320aa5eaa554692d1e
kfrn <knfrances@gmail.com>
2016-11-03 19:23:54 +1300

    Changed db call to use production env

--- db/cats.js
+++ db/cats.js
@@ -1,5 +1,6 @@
 var Knex = require('knex')
-var config = require('../knexfile')[ process.env.NODE_ENV || 'development' ]
+var config = require('../knexfile').production
+// [ process.env.NODE_ENV || 'development' ]
 var knex = Knex(config)
 
 module.exports = {
