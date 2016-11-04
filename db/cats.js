ecb0e4a4ca2a686a4cb01b431f8ab4a84d8b28c0
kfrn <knfrances@gmail.com>
2016-11-04 17:22:31 +1300

    Correct env variables

--- db/cats.js
+++ db/cats.js
@@ -1,6 +1,5 @@
 var Knex = require('knex')
-var config = require('../knexfile').production
-// [ process.env.NODE_ENV || 'development' ]
+var config = require('../knexfile')[ process.env.NODE_ENV || 'development' ]
 var knex = Knex(config)
 
 module.exports = {
--- knexfile.js
+++ knexfile.js
@@ -25,20 +25,6 @@ module.exports = {
     }
   },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'djqsbtxbnvlmtk',
-      password: '-sfC4Fq7cxgMjyH1-LXovi7DJl'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+  production: process.env.DATABASE_URL
 
 };
